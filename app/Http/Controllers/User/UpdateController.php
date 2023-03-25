370dec4b9a3e3670996fb641c5cb84691d7e9cd0
NWPro <shop@foxtron.ru>
2023-03-25 14:28:59 +0300

    edit profile image upload

--- app/Http/Controllers/User/UpdateController.php
+++ app/Http/Controllers/User/UpdateController.php
@@ -6,6 +6,9 @@ use App\Http\Requests\User\UpdateRequest;
 use App\Models\User;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
+use Intervention\Image\Facades\Image;
 
 class UpdateController extends Controller
 {
@@ -27,15 +30,24 @@ class UpdateController extends Controller
     public function __invoke(UpdateRequest $request)
     {
         $data = $request->validated();
+        $user = User::find(auth()->user()->getAuthIdentifier());
 
         if($data["image"] !== null)
         {
-            $imageName = time().'.'.$data["image"]->extension();
-            $data["image"]->storeAs('images', $imageName);
+            $imageName = hash('sha256', auth()->user()->getAuthIdentifier().time()).'.'.$data["image"]->extension();
+            $data["image"]->storeAs('images', $imageName, 'public');
+            $destinationPath = public_path('/thumbnail');
+            $img = Image::make($data["image"]->path());
+            $img->resize(200, 200, function ($constraint) {
+                $constraint->aspectRatio();
+            })->save($destinationPath.'/'.$imageName);
             $data["image_url"] = $imageName;
+            if($user->image_url !== null){
+                Storage::disk('public')->delete('images/'.$user->image_url);
+                File::delete('thumbnail/'.$user->image_url);
+            }
         }
         unset($data["image"]);
-        $user = User::find(auth()->user()->getAuthIdentifier());
         $user->update($data);
 
         return response()->json([
